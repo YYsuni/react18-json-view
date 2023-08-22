390db2e8e9352931409b7c98c107a5ce14a7205e
Suni <yysuni1001@gmail.com>
2023-08-22 22:16:11 +0800

    Finish add function

--- src/components/object-node.tsx
+++ src/components/object-node.tsx
@@ -1,10 +1,11 @@
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useRef, useState } from 'react'
 import { JsonViewContext } from './json-view'
 import { isObject } from '../utils'
 import { ReactComponent as AngleDownSVG } from '../svgs/angle-down.svg'
 import CopyButton from './copy-button'
 import NameValue from './name-value'
 import { ReactComponent as DeleteSVG } from '../svgs/trash.svg'
+import { ReactComponent as AddSVG } from '../svgs/add-square.svg'
 import { ReactComponent as DoneSVG } from '../svgs/done.svg'
 import { ReactComponent as CancelSVG } from '../svgs/cancel.svg'
 
@@ -17,15 +18,15 @@ interface Props {
 }
 
 export default function ObjectNode({ node, depth, name, parent, deleteHandle: _deleteSelf }: Props) {
-	const { collapsed, enableClipboard, collapseObjectsAfterLength, editable, onDelete, src } =
+	const { collapsed, enableClipboard, collapseObjectsAfterLength, editable, onDelete, src, onAdd } =
 		useContext(JsonViewContext)
 
 	const [nodeState, setNodeState] = useState(node)
 	const [fold, setFold] = useState(
 		collapsed === true ||
 			(typeof collapsed === 'number' && depth > collapsed) ||
-			(Array.isArray(node) && node.length > collapseObjectsAfterLength) ||
-			(isObject(node) && Object.keys(node).length > collapseObjectsAfterLength)
+			(Array.isArray(nodeState) && nodeState.length > collapseObjectsAfterLength) ||
+			(isObject(nodeState) && Object.keys(nodeState).length > collapseObjectsAfterLength)
 			? true
 			: false
 	)
@@ -46,38 +47,66 @@ export default function ObjectNode({ node, depth, name, parent, deleteHandle: _d
 	const deleteHandle = (indexOrName: number | string) => {
 		if (Array.isArray(nodeState)) {
 			nodeState.splice(+indexOrName, 1)
-			node.splice(+indexOrName, 1)
 			setNodeState([...nodeState])
 		} else if (nodeState) {
 			delete nodeState[indexOrName]
-			//@ts-ignore
-			delete node[indexOrName]
 			setNodeState({ ...nodeState })
 		}
 	}
 
 	// Delete self
 	const [deleting, setDeleting] = useState(false)
-	const isEditing = deleting
-
-	const cancel = () => {
-		setDeleting(false)
-	}
 	const deleteSelf = () => {
 		setDeleting(false)
 		if (_deleteSelf) _deleteSelf(name!)
 		if (onDelete)
-			onDelete({ value: node, depth, src, name: name!, parentType: Array.isArray(parent) ? 'array' : 'object' })
+			onDelete({ value: node, depth, src, indexOrName: name!, parentType: Array.isArray(parent) ? 'array' : 'object' })
+	}
+
+	// Add
+	const [adding, setAdding] = useState(false)
+	const inputRef = useRef<HTMLInputElement>(null)
+	const add = () => {
+		if (isObject(nodeState)) {
+			const inputName = inputRef.current?.value
+
+			if (inputName) {
+				;(nodeState as Record<string, any>)[inputName] = null
+				setNodeState({ ...nodeState })
+				if (inputRef.current) inputRef.current.value = ''
+				setAdding(false)
+
+				if (onAdd) onAdd({ indexOrName: inputName, depth, src, parentType: 'object' })
+			}
+		} else if (Array.isArray(nodeState)) {
+			nodeState.push(null)
+			setNodeState([...nodeState])
+			if (onAdd) onAdd({ indexOrName: nodeState.length - 1, depth, src, parentType: 'object' })
+		}
+		return {}
+	}
+
+	const isEditing = deleting || adding
+	const cancel = () => {
+		setDeleting(false)
+		setAdding(false)
 	}
 
 	const Icons = (
 		<>
 			{!fold && !isEditing && <AngleDownSVG onClick={() => setFold(true)} className='jv-chevron' />}
 
-			{isEditing && <DoneSVG className='json-view--edit' style={{ display: 'inline-block' }} onClick={deleteSelf} />}
+			{adding && isObject(nodeState) && <input className='json-view--input' placeholder='property' ref={inputRef} />}
+
+			{isEditing && (
+				<DoneSVG className='json-view--edit' style={{ display: 'inline-block' }} onClick={adding ? add : deleteSelf} />
+			)}
 			{isEditing && <CancelSVG className='json-view--edit' style={{ display: 'inline-block' }} onClick={cancel} />}
 
 			{!fold && !isEditing && enableClipboard && <CopyButton text={JSON.stringify(node)} />}
+			{!fold && !isEditing && editable && (
+				<AddSVG className='json-view--edit' onClick={() => (Array.isArray(nodeState) ? add() : setAdding(true))} />
+			)}
 			{!fold && !isEditing && editable && <DeleteSVG className='json-view--edit' onClick={() => setDeleting(true)} />}
 		</>
 	)
@@ -92,7 +121,14 @@ export default function ObjectNode({ node, depth, name, parent, deleteHandle: _d
 				{!fold ? (
 					<div className='jv-indent'>
 						{nodeState.map((n, i) => (
-							<NameValue key={i} name={i} value={n} depth={depth} parent={node} deleteHandle={deleteHandle} />
+							<NameValue
+								key={String(i) + String(n)}
+								name={i}
+								value={n}
+								depth={depth}
+								parent={node}
+								deleteHandle={deleteHandle}
+							/>
 						))}
 					</div>
 				) : (
