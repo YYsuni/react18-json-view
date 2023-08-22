5842c06edc4286b548349c9cb13ace86cb809688
Suni <YYsuni1001@gmail.com>
2023-08-22 17:21:31 +0800

    Finish edit function

--- src/components/name-value.tsx
+++ src/components/name-value.tsx
@@ -1,10 +1,33 @@
+import { useContext } from 'react'
 import JsonNode from './json-node'
+import { JsonViewContext } from './json-view'
 
-export default function NameValue({ name, value, depth }: { name: number | string; value: any; depth: number }) {
+interface Props {
+	name: number | string
+	value: any
+	depth: number
+	parent?: Record<string, any> | Array<any>
+}
+
+export default function NameValue({ name, value, depth, parent }: Props) {
+	const { onEdit, src } = useContext(JsonViewContext)
+
+	const editValue = (newValue: any) => {
+		if (Array.isArray(parent)) {
+			parent[+name] = newValue
+
+			if (onEdit) onEdit({ newValue, oldValue: value, depth, src, name, parentType: 'array' })
+		} else if (parent) {
+			parent[name] = value
+
+			if (onEdit) onEdit({ newValue, oldValue: value, depth, src, name, parentType: 'object' })
+		}
+	}
+
 	return (
 		<div className='json-view--pair'>
 			<span className={typeof name === 'number' ? 'json-view--index' : 'json-view--property'}>{name}</span>:{' '}
-			<JsonNode node={value} depth={depth + 1} />
+			<JsonNode node={value} depth={depth + 1} editValue={editValue} />
 		</div>
 	)
 }
